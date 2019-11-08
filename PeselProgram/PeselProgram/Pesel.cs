3d66dd52a6a3728a3f3fc2ab3d2a87403b6f7ad6
MattShah <mzawiski97@gmail.com>
2019-11-08 21:25:28 +0100

    Added Main Panel GUI
    Added text boxes, buttons and other GUI elements to Main Panel

---
+++ PeselProgram/PeselProgram/Pesel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeselProgram
+{
+    class Pesel
+    {
+        private string pesel_number;
+        private bool is_valid;
+        private Gender gender;
+        private DateTime birth_day;
+        
+        public Pesel(string number)
+        {
+            pesel_number = number;
+        }
+
+        public bool IsValid
+        {
+            get { return is_valid; }
+        }
+
+        public Gender GetGender() => gender;
+
+        public DateTime GetBirthDate() => birth_day;
+
+
+    }
+}
