124669d38f210e2079f2d2a50ab5efc3aab9d4ff
MattShah <mzawiski97@gmail.com>
2019-11-14 22:08:19 +0100

    Pesel Class and Main Panel GUI connection

--- PeselProgram/PeselProgram/GUI/MainPanel.xaml.cs
+++ PeselProgram/PeselProgram/GUI/MainPanel.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +21,101 @@ namespace PeselProgram.GUI
     /// </summary>
     public partial class MainPanel : Page
     {
+        List<TextBox> peselBoxes;
         public MainPanel()
         {
             InitializeComponent();
+            peselBoxes = new List<TextBox>();
+            peselBoxes.Add(PeselNumberBox1);
+            peselBoxes.Add(PeselNumberBox2);
+            peselBoxes.Add(PeselNumberBox3);
+            peselBoxes.Add(PeselNumberBox4);
+            peselBoxes.Add(PeselNumberBox5);
+            peselBoxes.Add(PeselNumberBox6);
+            peselBoxes.Add(PeselNumberBox7);
+            peselBoxes.Add(PeselNumberBox8);
+            peselBoxes.Add(PeselNumberBox9);
+            peselBoxes.Add(PeselNumberBox10);
+            peselBoxes.Add(PeselNumberBox11);
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void PeselInputBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = PeselInputBox.Text;
+            if (peselBoxes != null && PeselInputBox != null)
+            {
+                for (int i = 0; i < peselBoxes.Count; i++)
+                {
+                    if (i >= text.Length)
+                    {
+                        peselBoxes[i].Text = "";
+                        continue;
+                    }
+
+
+                    peselBoxes[i].Text = text[i].ToString();
+                }
+            }
+        }
+
+        private void PeselInputBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            InfoLabel.Content = "";
+            string text = PeselInputBox.Text;
+            if (text.Length < 11)
+            {
+                InfoLabel.Content = "Niepełny numer pesel";
+                int tl = text.Length;
+                for (int i = 0; i < 12 - tl; i++)
+                    text += "_ ";
+                FinalPeselTextBox.Content = text;
+                FinalBirthDayTextBox.Content = "";
+                FinalGenderTexBox.Content = "";
+                FinalCheckSumTextBox.Content = "";
+                return;
+            }
+                
+
+            Pesel pesel = new Pesel(text);
+
+            if (!pesel.IsCorrect)
+            {
+                InfoLabel.Content = "Niepoprawna składnia numeru pesel";
+                FinalPeselTextBox.Content = text;
+                FinalBirthDayTextBox.Content = "";
+                FinalGenderTexBox.Content = "";
+                FinalCheckSumTextBox.Content = "";
+                return;
+            }
+                
+            if (!pesel.IsBirthdateCorrect)
+            {
+                InfoLabel.Content = "Niepoprawna data urodzenia";
+                FinalPeselTextBox.Content = text;
+                FinalBirthDayTextBox.Content = "Incorrect";
+                FinalGenderTexBox.Content = "";
+                FinalCheckSumTextBox.Content = "";
+                return;
+            }
+                
+
+
+
+            FinalPeselTextBox.Content = text;
+            FinalBirthDayTextBox.Content = pesel.GetBirthDate().ToString();
+            FinalGenderTexBox.Content = pesel.GetGender().ToString();
+            FinalCheckSumTextBox.Content = pesel.IsChecksumCorrect.ToString();
         }
     }
 }
--- PeselProgram/PeselProgram/Model/Pesel.cs
+++ PeselProgram/PeselProgram/Model/Pesel.cs
@@ -9,25 +9,30 @@ namespace PeselProgram
     class Pesel
     {
         private string pesel_number;
-        private bool is_valid;
+        private bool is_checksum_correct;
+        private bool is_birthdate_correct;
+
         private bool is_correct;
         private Gender gender;
-        private DateTime birth_day;
+        private Model.BirthDate birth_date;
         private static int[] wages = new int[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
         
         public Pesel(string number)
         {
             pesel_number = number;
+
             is_correct = CheckIfCorrect(number);
             if (!is_correct)
             {
-                is_valid = false;
-                birth_day = new DateTime();
+                is_checksum_correct = false;
+                is_birthdate_correct = false;
+                birth_date = new Model.BirthDate();
                 gender = Gender.Unknown;
                 return;
             }
-            is_valid = ValidateNumber(number);
-            birth_day = CheckBirthDate(number);
+
+            is_checksum_correct = ValidateChecksumNumber(number);
+            birth_date = CheckBirthDate(number);
             gender = CheckGender(number);
         }
 
@@ -35,35 +40,74 @@ namespace PeselProgram
         {
             get { return is_correct; }
         }
-        public bool IsValid
+        public bool IsChecksumCorrect
         {
-            get { return is_valid; }
+            get { return is_checksum_correct; }
+        }
+        public bool IsBirthdateCorrect
+        {
+            get { return is_birthdate_correct; }
         }
 
         public Gender GetGender() => gender;
 
-        public DateTime GetBirthDate() => birth_day;
+        public string GetBirthDate() => birth_date.GetStringDate();
 
         private Gender CheckGender(string pesel)
         {
-            // Check length if correct
-            if(pesel.Length == 1)
-            {
-                if ((int)pesel[9] % 2 == 0)
-                    return Gender.Female;
-                else
-                    return Gender.Male;
-            }
+            if (!IsCorrect)
+                return Gender.Unknown;
 
-            return Gender.Unknown;
+
+            if (int.Parse(pesel[9].ToString()) % 2 == 0)
+                    return Gender.Female;
+            else
+                    return Gender.Male;           
+
         }
 
-        private DateTime CheckBirthDate(string pesel)
+        private Model.BirthDate CheckBirthDate(string pesel)
         {
-            int day = (int)(pesel[4] + pesel[5]);
-            int month = (int)(pesel[2] + pesel[3]);
-            int year = (int)(pesel[0] + pesel[1]);
-            DateTime bd = new DateTime(day: day, month: month, year: year);
+            int year = 0;
+            int day = int.Parse("0" + pesel[4] + pesel[5]);
+            int month = int.Parse("0" + pesel[2] + pesel[3]);
+            if(month > 80 && month < 93)
+            {
+                month -= 80;
+                year = 1800;
+            }
+            else if (month > 0 && month < 13)
+            {
+                month -= 0;
+                year = 1900;
+            }
+            else if (month > 20 && month < 33)
+            {
+                month -= 20;
+                year = 2000;
+            }
+            else if (month > 40 && month < 53)
+            {
+                month -= 40;
+                year = 2100;
+            }
+            else if (month > 60 && month < 73)
+            {
+                month -= 60;
+                year = 2200;
+            }
+            else
+            {
+                is_birthdate_correct = false;
+                return new Model.BirthDate();
+            }
+
+            year += int.Parse( "0" + pesel[0] + pesel[1]);
+
+
+            Model.BirthDate bd = new Model.BirthDate(day, month, year);
+            is_birthdate_correct = bd.IsCorrect();
+
             return bd;
         }
 
@@ -74,18 +118,18 @@ namespace PeselProgram
 
             return (isIntString && has11digits);
         }
-        private bool ValidateNumber(string pesel)
+        private bool ValidateChecksumNumber(string pesel)
         {
             int sum = 0;
             for(int i = 0; i<10; i++)
             {
-                string nums = ((int)pesel[i] * wages[i]).ToString();
-                sum += (int)(nums[nums.Length - 1]);
+                int nums = int.Parse(pesel[i].ToString()) * wages[i];
+                sum += nums%10;
             }
 
-            int final_number = 10 - (int)(sum.ToString()[sum.ToString().Length - 1]);
+            int final_number = 10 - sum%10;
 
-            if (final_number == (int)pesel[10])
+            if (final_number == int.Parse(pesel[10].ToString()))
                 return true;
 
             return false;
